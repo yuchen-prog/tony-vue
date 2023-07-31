d15d7c4f3564f71b42a4fee9d1ffe1146dbc62bd
dddddrose <ddrose1206@gmail.com>
2023-08-01 00:14:04 +0800

    改善响应式结构

--- src/reactive.ts
+++ src/reactive.ts
@@ -1,17 +1,43 @@
-import { activeEffect } from "./effect";
+import { activeEffect, effect } from "./effect";
 
 // 设计一个bucket用于存effectFn
-let bucket = new Set();
+let bucket = new WeakMap();
+
+/**
+ * 
+ target1
+     └── text1
+         └── effectFn1
+ target2
+     └── text2
+         └── effectFn2
+ * @param obj 
+ * @returns 
+ */
 
 export function reactive(obj) {
     return new Proxy(obj, {
         get(target, key) {
-            bucket.add(activeEffect)
+            // 依赖收集
+            if (!activeEffect) return target[key]
+            let depsMap = bucket.get(target as any)
+            if (!depsMap) {
+                bucket.set(target as any, (depsMap = new Map()));
+            }
+            let deps = depsMap.get(key);
+            if (!deps) {
+                depsMap.set(key, (deps = new Set()));
+            }
+            deps.add(activeEffect)
             return target[key]
         },
         set(target, key, value) {
+            // 触发依赖
             target[key] = value;
-            bucket.forEach((fn: any) => fn());
+            const depsMap = bucket.get(target);
+            if (!depsMap) return true;
+            const deps = depsMap.get(key);
+            deps && deps.forEach((fn: any) => fn());
             return true
         }
     })
--- src/tests/effect.spec.ts
+++ src/tests/effect.spec.ts
@@ -14,15 +14,30 @@ import { reactive } from "../reactive";
  */
 
 describe('effect', () => {
-    it('init', () => {
-        const user = reactive({ age: 10 });
-        let newAge;
-        effect(() => newAge = user.age + 1);
-        expect(newAge).toBe(11);
+  it('init', () => {
+    const user = reactive({ age: 10 });
+    let newAge;
+    effect(() => newAge = user.age + 1);
+    expect(newAge).toBe(11);
 
-        // update
-        user.age++;
-        expect(newAge).toBe(12);
+    // update
+    user.age++;
+    expect(newAge).toBe(12);
+  }),
+
+    it('noEffect', () => {
+      const user = reactive({ age: 10, len: 20 });
+      // 记录一下effect内部的执行次数
+      let i = 0;
+      let newAge;
+      effect(() => {
+        newAge = user.age + 1;
+        i++;
+      });
+      expect(newAge).toBe(11);
+      expect(i).toBe(1);
+      user.len = 30;
+      expect(i).toBe(1);
     })
 
 })
