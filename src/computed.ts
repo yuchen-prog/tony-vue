7d602391010d0a3fc491e986fc0551d67a354798
dddddrose <ddrose1206@gmail.com>
2023-08-13 20:58:31 +0800

    init watch function

--- src/computed.ts
+++ src/computed.ts
@@ -25,3 +25,33 @@ export function computed(getter) {
 
     return res;
 }
+
+
+export function watch(obj, cb) {
+    const isGetter = typeof obj !== 'object'
+    let newVal;
+    let oldVal;
+    const effectFn = effect(isGetter ? obj : () => traverse(obj), {
+        lazy: true,
+        scheduler() {
+            // 触发依赖时的调用
+            newVal = effectFn()
+            cb(newVal, oldVal);
+            oldVal = newVal
+        }
+    });
+    // 第一次获取初始值
+    oldVal = effectFn();
+}
+
+function traverse(value, seen = new Set()) {
+    // 递归调用每个属性
+    if (typeof value !== 'object' || !value || seen.has(value)) {
+        return
+    }
+    seen.add(value)
+    for (const key in value) {
+        traverse(value[key], seen)
+    }
+    return value;
+}
--- src/tests/compute.spec.ts
+++ src/tests/compute.spec.ts
@@ -1,44 +1,77 @@
 import { reactive } from "../reactive"
-import { computed } from "../computed"
+import { computed, watch } from "../computed"
 import { effect } from "../effect"
 
 describe('computed', () => {
-    it('should return updated value', () => {
-        const value = reactive({ foo: 1 })
-        const cValue = computed(() => value.foo)
-        expect(cValue.value).toBe(1)
-        value.foo++;
-        expect(cValue.value).toBe(2)
-    }),
+    // it('should return updated value', () => {
+    //     const value = reactive({ foo: 1 })
+    //     const cValue = computed(() => value.foo)
+    //     expect(cValue.value).toBe(1)
+    //     value.foo++;
+    //     expect(cValue.value).toBe(2)
+    // }),
 
-        it('sum', () => {
-            const obj = reactive({ foo: 1, bar: 2 })
-            const sumRes = computed(() => obj.foo + obj.bar)
-            expect(sumRes.value).toBe(3);  // 3
-        }),
+    //     it('sum', () => {
+    //         const obj = reactive({ foo: 1, bar: 2 })
+    //         const sumRes = computed(() => obj.foo + obj.bar)
+    //         expect(sumRes.value).toBe(3);  // 3
+    //     }),
 
-        it('nested computed', () => {
-            const value = reactive({ foo: 1 })
-            const cValue = computed(() => value.foo)
-            let val = 0
-            effect(() => {
-                val = cValue.value
+    //     it('nested computed', () => {
+    //         const value = reactive({ foo: 1 })
+    //         const cValue = computed(() => value.foo)
+    //         let val = 0
+    //         effect(() => {
+    //             val = cValue.value
+    //         })
+    //         expect(val).toBe(1)
+    //         value.foo++;
+    //         expect(cValue.value).toBe(2)
+    //         expect(val).toBe(2)
+    //     }),
+
+    //     it('should work when chained', () => {
+    //         const value = reactive({ foo: 0 })
+    //         const c1 = computed(() => value.foo)
+    //         const c2 = computed(() => c1.value + 1)
+    //         expect(c2.value).toBe(1)
+    //         expect(c1.value).toBe(0)
+    //         value.foo++
+    //         expect(c2.value).toBe(2)
+    //         expect(c1.value).toBe(1)
+    //     }),
+
+    //     it("watch main", () => {
+    //         const obj = reactive({ foo: 0 });
+    //         let i = 0;
+    //         watch(obj, () => {
+    //             i++;
+    //         });
+    //         obj.foo = 1;
+    //         expect(i).toBe(1);
+    //     }),
+
+    //     it("watch getter", () => {
+    //         const obj = reactive({ foo: 0 });
+    //         let i = 0;
+    //         watch(() => obj.foo, () => {
+    //             i++;
+    //         })
+    //         obj.foo = 2;
+    //         expect(i).toBe(1)
+    //     }),
+
+        it("watch newVal oldVal", () => {
+            const obj = reactive({ foo: 0 });
+            let newVal = 0;
+            let oldVal = 0;
+            watch(() => obj.foo, (newValue, oldValue) => {
+                newVal = newValue;
+                oldVal = oldValue;
             })
-            expect(val).toBe(1)
-            value.foo++;
-            expect(cValue.value).toBe(2)
-            expect(val).toBe(2)
-        }),
-
-        it('should work when chained', () => {
-            const value = reactive({ foo: 0 })
-            const c1 = computed(() => value.foo)
-            const c2 = computed(() => c1.value + 1)
-            expect(c2.value).toBe(1)
-            expect(c1.value).toBe(0)
-            value.foo++
-            expect(c2.value).toBe(2)
-            expect(c1.value).toBe(1)
+            obj.foo = 2;
+            expect(newVal).toBe(2);
+            expect(oldVal).toBe(0);
         })
 
 }) 
